df2635104a6448f55ccbc367717259354432b93f
Hao079231 <haotrinh2003.com@gmail.com>
2026-04-23 14:25:29 +0700

    fix code simulation enrollment

--- source/user-base-auth/src/main/java/com/base/auth/config/ResourceServerConfig.java
+++ source/user-base-auth/src/main/java/com/base/auth/config/ResourceServerConfig.java
@@ -45,6 +45,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .antMatchers("/v1/category/auto-complete").permitAll()
                 .antMatchers("/v1/simulation/guest_list", "/v1/simulation/guest_get/**").permitAll()
                 .antMatchers("/v1/task/student_list").permitAll()
+                .antMatchers("/v1/organization/guest_list").permitAll()
                 .antMatchers("/**").authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
